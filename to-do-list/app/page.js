ab6eaa23687bc33f043a41b40db0c3e3306538bf
evelyndisa <evelyndisa@icloud.com>
2024-06-17 21:33:22 -0300

    add method date

--- to-do-list/app/page.js
+++ to-do-list/app/page.js
@@ -1,7 +1,43 @@
 import "./page.css";
 
 export default function Home() {
+
+  const date = new Date(); 
+
+  const dayOfWeek = date.getDay(); 
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const day = days[dayOfWeek];
+
+  const dayOfMonth = date.getDate();
+
+  const getmonth = date.getMonth()
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const month = months[getmonth]
+
+
   return (
-    <></>
+    <div className="container">
+      <div className="date-selector">
+        <div className="date">
+          <p className="day">{`${day}, ${dayOfMonth}`}</p>
+          <p className="month">{month}</p>
+        </div>
+        <button className="button-mode-dark"></button>
+      </div>
+      <form>
+        <input type="text" id="input" name="input"></input>
+        <button></button>
+      </form>
+      <div>
+        <div>
+          <p></p>
+          <p></p>
+        </div>
+        <div>
+          <input type="checkbox" id="checkbox" name="checkbox"></input>
+          <button></button>
+        </div>
+      </div>
+    </div>
   );
 }
